6bee4e359c76d86039701973a6d87643f8a8c3e3
lynhtran <lynht273@gmail.com>
2024-04-21 20:52:31 -0700

    anotha one

--- src/components/Home/projects.js
+++ src/components/Home/projects.js
@@ -6,6 +6,23 @@ import wateredDown from "../../images/watered-down.png";
 const Projects = () => {
   const projects = [
     {
+      title: "This Website",
+      year: 2024,
+      description: `You're looking at it.`,
+      imageSrc:
+        "https://i0.wp.com/www.printmag.com/wp-content/uploads/2021/02/4cbe8d_f1ed2800a49649848102c68fc5a66e53mv2.gif?fit=476%2C280&ssl=1",
+      href: "https://github.com/LynhTran/LynhTran.github.io",
+      skills: [
+        "React.js",
+        "JS",
+        "TailwindCSS",
+        "Gatsby",
+        "Yarn",
+        "HTML",
+        "GitHub Pages",
+      ],
+    },
+    {
       title: "Dory Defense",
       year: 2022,
       description: `A 24 hackathon project that placed 1st at Minnehack 2022
